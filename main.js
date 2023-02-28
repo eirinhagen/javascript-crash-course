1adce5f2657e24498ea386f9dbd1b46174f6c6b6
Jane <evgeniiatsyp13@gmail.com>
2023-02-28 14:18:41 +0100

    example pf how to add variable in the string

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-let age = 20;
-console.log(age)
-age = 21;
-console.log(age)
+const kisa = "Eirin";
+const age = 25;
 
-
-const favAnimals = "dogs";
-favAnimals="cats";
-console.log(favAnimals);
-
+console.log("My name is " + kisa + " and I am " + age + " years old");
+console.log(`My name is ${kisa} and I am ${age} years old `);
+const hello = `My name is ${kisa} and I am ${age} years old `
+console.log(hello)
