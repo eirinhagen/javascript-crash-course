8b530e87d8c3bb3edc63fac1c64bb252e87d1cd3
Jane <evgeniiatsyp13@gmail.com>
2023-03-02 13:55:30 +0100

    examples of arrays

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const s = "Hello World!";
-console.log(s.length) // property
-console.log(s.toUpperCase()); // method
-console.log(s.substring(0,5))
-const k = 'tech, comp, it'
-console.log(k.split(', ')); // split into array (different words with comma)
+//arrays
 
+const kot = ["eyes","nose","whiskers","mouth"]
+console.log(kot)
+console.log(kot[1]) //to access one value from array
+
+kot[4] = "ears"; // to add smth to array
+ kot.push("brows"); //to add  value to the end of array, it is method
+console.log(kot)
+
+kot.pop(); // delete value from end
+console.log(kot)
+
+console.log(kot.indexOf("eyes")) // to know index of value
