a486da80b472e1c87fb6980cfc8cd392b3ec9145
Jane <evgeniiatsyp13@gmail.com>
2023-03-02 15:11:38 +0100

    example pf shortcut to ifelse?

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ if(x === 10) {
   console.log('x is less than 10')
 } 
 // simple example of if else statement
+ //|| two of statements are true
+// && one statement is true
+
+const k = 10;
+const color = k >5 ? "red" : "blue" ;
+console.log(color) // shortcut to the if else
