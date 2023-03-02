ba9b4e7e622c4ca26e23aa0b07c20b15bef1156a
Jane <evgeniiatsyp13@gmail.com>
2023-03-02 14:52:09 +0100

    example oof forEach, filter and map

--- main.js
+++ main.js
@@ -5,19 +5,39 @@ const todos = [
         isCompleted: true
     },
     {
-        id: 1,
+        id: 2,
         text: "Meeting John",
         isCompleted: false
     }
 ]
- //Loops
+ //Loop through array
 
- for(let i=0; i<10; i++){
-    console.log(i)
+ for(let i=0; i<todos.length; i++){
+    console.log(todos[i].text);
  }
 
- let i = 0;
- while(i<10) {
-    console.log (i);
-    i++;
- }
+
+ for (let todo of todos){
+    console.log(todo.text)
+ } // more simple way to loop through array
+
+ todos.forEach(function(todo){
+    console.log(todo.text);
+ });  // todos-name of array, todo- name of the variable for each value in the array
+
+ const todoText = todos.map(function(todo){
+    return todo.text});
+
+    todos.map(function(todo){
+        console.log(todo.text)
+    });
+
+    console.log (todoText) //map method of goingh through an array
+
+    const todoCompleted = todos.filter(function(todo){
+        return todo.isCompleted === true;
+    }).map(function(todo){
+        return todo.text
+    });
+     console.log(todoCompleted) // its so awesome....
+     
