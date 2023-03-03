74aa3d4195eb2a32887963d7840f5cba203cd7bb
Jane <evgeniiatsyp13@gmail.com>
2023-03-03 11:25:34 +0100

    example of manupulating the DOM

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-function Person(firstName,lastName,dob){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.dob = new Date(dob)
-}
 
-const person1 = new Person("John", "Hagen","1-24-1992")
+// single element
+ const form = document.getElementById('my-form');
+ console.log(form)
 
-const person2 = new Person("Eirin","Hagen","2-13-1998")
+console.log(document.querySelector('.container'))
+const x = document.querySelector('h1');
+console.log(x)
 
- console.log(person2)
- 
+//multiple selector
+console.log( document.querySelectorAll('.item'));
+const items =  document.querySelectorAll('.item')
+
+items.forEach((item)=>console.log(item));
+
+
+const ul = document.querySelector('.items');
+// ul.remove();
+// ul.lastElementChild.remove()
+
+ul.firstElementChild.textContent = "Hello";
+ul.children[1].innerHTML = "Eirin" // the other way to change the text
+ul.lastElementChild.innerHTML = "<h1>Hello!</h1>";
+
+const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+btn.addEventListener('click',(e) => { 
+    e.preventDefault();                                            // first "click " is event and second is the function, e is event-parameter
+   // console.log(e.target.className); // target is an actual element
+   document.querySelector('#my-form').style.background= '#ccc'
+   document.querySelector('body').classList.add('bg-dark')
+   document.querySelector('.items').lastElementChild.innerHTML = '<h1>myaw</h1>'
+});
+
